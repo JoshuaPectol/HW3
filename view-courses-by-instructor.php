6f70e4a86c0d034eed18dd055052e01a38454965
JoshuaPectol <61020071+JoshuaPectol@users.noreply.github.com>
2023-10-24 16:18:23 -0500

    Update view-courses-by-instructor.php

--- view-courses-by-instructor.php
+++ view-courses-by-instructor.php
@@ -1,1 +1,30 @@
+<h1>Courses By Instructor</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <th>ID</th>
+      <th>Number</th>
+      <th>Description</th>
+      <th>Semester</th>
+      <th>Room</th>
+      <th>Day/Time</th>
+    </thead>
+    <tbody>
+<?php
+while($course = $courses->fetch_assoc()){
+?>
+  <tr>
+    <td><?php echo $course['course_id'];?></td>
+    <td><?php echo $course['course_number'];?></td>
+    <td><?php echo $course['course_description'];?></td>
+    <td><?php echo $course['semester'];?></td>
+    <td><?php echo $course['room'];?></td>
+    <td><?php echo $course['day_time'];?></td>
+  </tr>
+<?php
+}
+?>
 
+    </tbody>
+  </table>
+</div>
