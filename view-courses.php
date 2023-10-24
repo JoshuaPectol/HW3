ba62e77b3bce91664ec19fe5c1155e153520a196
JoshuaPectol <61020071+JoshuaPectol@users.noreply.github.com>
2023-10-24 16:00:58 -0500

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -1,1 +1,24 @@
+<h1>Instructors</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <th>ID</th>
+      <th>Number</th>
+      <th>Description</th>
+    </thead>
+    <tbody>
+<?php
+while($course = $courses->fetch_assoc()){
+?>
+  <tr>
+    <td><?php echo $course['course_id'];?></td>
+    <td><?php echo $course['course_number'];?></td>
+    <td><?php echo $course['course_description'];?></td>
+  </tr>
+<?php
+}
+?>
 
+    </tbody>
+  </table>
+</div>
