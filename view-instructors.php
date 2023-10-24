ac50e0db2e5139858508d538c46126d48a42071f
JoshuaPectol <61020071+JoshuaPectol@users.noreply.github.com>
2023-10-24 15:39:50 -0500

    Create view-instructors.php

---
+++ view-instructors.php
@@ -0,0 +1,21 @@
+<h1>Instructors</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Office</th>
+    </thead>
+    <tbody>
+<?php
+while($instructor = $instructors->fetch-assoc()){
+?>
+  <tr>
+    <td><?php echo $instructor['instructor_id'];?></td>
+    <td><?php echo $instructor['instructor_name'];?></td>
+    <td><?php echo $instructor['office_number'];?></td>
+  </tr>
+
+    </tbody>
+  </table>
+</div>
