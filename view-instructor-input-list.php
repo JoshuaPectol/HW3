419273eb9477b75c0de388e05823d01711182d79
JoshuaPectol <61020071+JoshuaPectol@users.noreply.github.com>
2023-10-25 11:47:07 -0500

    Update view-instructor-input-list.php

--- view-instructor-input-list.php
+++ view-instructor-input-list.php
@@ -1,8 +1,11 @@
 <select class="form-select" id="iid" name="iid">
 <?php
 while ($instructorItem = $instructorList ->fetch_assoc()){
+  $selText = "";
+  if($selectedInstructor == $instructorItem['instructor_id']{
+     $selText = "selected";
 ?>
-  <option value="<?php echo $instructor['instructor_id']; ?>"><?php echo $instructor['instructor_name']; ?></option>
+  <option value="<?php echo $instructor['instructor_id']; ?>" <?=$selText?>><?php echo $instructor['instructor_name']; ?></option>
 <?php
 }
 ?>
